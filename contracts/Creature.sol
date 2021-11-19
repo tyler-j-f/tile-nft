f02d8d97830370be207f2c705a9af074b29393d6
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-11-19 15:42:14 -0500

    added new token contract methods and events to emit

--- contracts/Creature.sol
+++ contracts/Creature.sol
@@ -9,6 +9,19 @@ import "./ERC721Tradable.sol";
  * Creature - a contract for my non-fungible creatures.
  */
 contract Creature is ERC721Tradable {
+
+    event ColorSet(
+        uint256 indexed tokenId,
+        uint256 indexed index,
+        uint256 indexed rgbValue
+    );
+
+    event EmojiSet(
+        uint256 indexed tokenId,
+        uint256 indexed index,
+        uint256 indexed unicodeValue
+    );
+
     constructor(address _proxyRegistryAddress)
         ERC721Tradable("Creature", "OSC", _proxyRegistryAddress)
     {}
@@ -20,4 +33,19 @@ contract Creature is ERC721Tradable {
     function contractURI() public pure returns (string memory) {
         return "http://34.150.230.209/api/contract/get";
     }
+
+    function setColor() public onlyOwner {
+        uint256 tokenId = 1;
+        uint256 index = 2;
+        uint256 rgbValue = 3;
+        emit ColorSet(tokenId, index, rgbValue);
+    }
+
+    function setEmoji() public onlyOwner {
+        uint256 tokenId = 1;
+        uint256 index = 2;
+        uint256 rgbValue = 3;
+        emit EmojiSet(tokenId, index, unicodeValue);
+    }
+
 }
