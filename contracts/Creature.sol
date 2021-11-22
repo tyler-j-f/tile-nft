be832620585bfd027c6405d357afdaeba6e57cc7
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-11-22 06:44:49 -0500

    Revert "Added mergeMintTo"
    This reverts commit a88c1a76d60be60ea998917d7f14cad2e1779ae6.

--- contracts/Creature.sol
+++ contracts/Creature.sol
@@ -50,14 +50,8 @@ contract Creature is ERC721Tradable {
             address burnTokenAddress = 0x0123456789012345678901234567890123456789;
             ERC721.transferFrom(sender, burnTokenAddress, tokenId1);
             ERC721.transferFrom(sender, burnTokenAddress, tokenId2);
-            mergeMintTo(sender);
+            ERC721Tradable.mintTo(sender);
         }
-    }
-
-    function mergeMintTo(address _to) private {
-        uint256 newTokenId = _getNextTokenId();
-        _mint(_to, newTokenId);
-        _incrementTokenId();
     }
 
 }
