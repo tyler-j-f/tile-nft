c26a6c02afcd9d23943c27d578391dfc93e595c1
Tyler Fitzgerald <tylerjf1993@gmail.com>
2021-11-22 15:40:30 -0500

    mergeMint max supply changes

--- contracts/Creature.sol
+++ contracts/Creature.sol
@@ -57,9 +57,9 @@ contract Creature is ERC721Tradable {
     function merge(uint256 tokenId1,  uint256 tokenId2) public {
         address sender = _msgSender();
         require(ERC721.ownerOf(tokenId1) == sender && ERC721.ownerOf(tokenId2) == sender);
+        uint256 newTokenId = totalSupply() + 1;
         _burn(tokenId1);
         _burn(tokenId2);
-        uint256 newTokenId = totalSupply() + 1;
         _mint(sender, newTokenId);
         emit MergeMint(tokenId1, tokenId2, newTokenId);
     }
